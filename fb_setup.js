d1bbecdeb594538cf58a32a14acbc73c01e98f92
23501ap <23501ap@hvhs.school.nz>
2026-06-08 01:28:36 +0000

    setup

--- fb_setup.js
+++ fb_setup.js
@@ -1,11 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCRFEEaBxknFnx2RI4Tl5eKz0_9HUdQAZg",
   authDomain: "athul-12database-project.firebaseapp.com",
@@ -16,6 +9,5 @@ const firebaseConfig = {
   measurementId: "G-3SSMLK5SC7"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
+console.log(firebase)
