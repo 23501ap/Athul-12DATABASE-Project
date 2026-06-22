4998adac6c794ad1784e50cca7a46fcaca9e6bc0
23501ap <23501ap@hvhs.school.nz>
2026-06-22 01:28:51 +0000

    ipg

--- script.js
+++ script.js
@@ -1,41 +1,45 @@
 
-var GLOBAL_user; // Google's user object
-
-// Set up a listener for the login state of the user.
+var GLOBAL_user;
+var authenticationListener;
 function fb_login() {
     authenticationListener = firebase.auth().onAuthStateChanged(fb_handleLogin);
 }
-
-// Run when the login state of the user changes.
 function fb_handleLogin(_user) {
     if (_user) {
         console.log("User is logged in")
-        GLOBAL_user = _user; // Save the user object to a global variable
-        window.location.href = "Username.html" // Move to other page
+        GLOBAL_user = _user;
+        firebase.database().ref('/Games/users/' + GLOBAL_user.uid).update(
+            {
+                name: GLOBAL_user.displayName
+            }
+        );
+        window.location.href = "home.html" //Moving to Home Page
+
     } else {
         console.log("User is NOT logged in - Starting the popup process")
         fb_popupLogin();
 
     }
-
 }
-
-// Run the Google login popup
 function fb_popupLogin() {
     var provider = new firebase.auth.GoogleAuthProvider();
 
     firebase.auth().signInWithPopup(provider).then((result) => {
-        GLOBAL_user = result.user; // Save the user object to a global variable
+        GLOBAL_user = result.user; //Save the user details object to a global variable
         console.log("User has logged in")
-        window.location.href.once = "Username.html" // Moved to other page
+        window.location.href = "Home.html" //Moving to Home Page
     });
 }
 
+var authenticantionListner
+function fb_login() {
+    authenticationListener = firebase.auth().onAuthStateChanged(fb_handleLogin);
+}
 function fb_logout() {
-    // this line turns off the listener
+    authenticationListener = null; // this line turns off the listener
     firebase.auth().signOut();
-    console.log("Successfully logged out")
-    window.location.href = "index.html";
+    console.log("logged out successfully")
+    window.location.href = "index.html" //Moving back to Login Page
 }
 
 function Game01() {
@@ -60,8 +64,9 @@ function HighScores() { // setting up the database for high scores
         console.log(snapshot.val());
     });
     function writeHighScores() {
-        firebase.database().ref('/').set({
-            Game01: {
+        firebase.database().ref('/Games').set({
+            Games: {
+
                 users: {
                     "user1": {
                         name: "Athul",
@@ -88,49 +93,12 @@ function HighScores() { // setting up the database for high scores
 
                 }
             }
-        })
+
+        });
     }
 };
 
 
-function Submit_1(event) {
-    const form = document.getElementById('myForm');
-    const username = form.Username.value;
-    const name = form.name.value;
-    const age = form.Age.value;
-
-    // Prevent standard page reload
-    event.preventDefault();
-
-    // Reference to the specific user in the database
-    const userRef = firebase.database().ref('/game1/users/' + username);
-
-    // Check if the username already exists
-    userRef.once('value').then((snapshot) => {
-        if (snapshot.exists()) {
-            alert("Error: Username already taken. Please choose another one.");
-        } else {
-
-            userRef.set({
-                name: name,
-                age: age
-            }).then(() => {
-                console.log("Form submitted successfully!");
-
-                // Call the high score function here before leaving the page
-                // This initializes their score to 0 using their custom username
-                firebase.database().ref('/Game01/users/' + username).set({
-                    name: name,
-                    score: 0
-                }).then(() => {
-                    window.location.href = "home.html";
-                });
-            });
-        }
-    }).catch((error) => {
-        console.error("Error checking username: ", error);
-    });
-};
 
 
 //function Submit_1(event) {
@@ -173,4 +141,31 @@ function HighScores() {
     }
 }
 
+function Submit_1() {
+    //Check if the user is logged in
+    if (!GLOBAL_user) {
+        alert("Please log in before submitting the form!");
+        return;
+    }
+    // Get the form data
+    const Yourname = document.getElementById("Your Name").value;
+    const Age = document.getElementById("Age").value;
+    const Username = document.getElementById("Username").value;
+    console.log("Here")
+    console.log(Username)
 
+    console.log(GLOBAL_user.displayName + " likes " + Username + " They have it " + Yourname + " times")
+    firebase.database().ref('/Games/users/' + GLOBAL_user.uid).update(
+        {
+            name: Yourname,
+            age: Age,
+            username: Username
+        }
+    )
+
+    HTML_OUTPUT.innerHTML = GLOBAL_user.displayName + " likes " + Username + " They have it " + Yourname + " times"
+}
+function fb_readError(error) {
+    console.log("There was an error reading reading the message");
+    console.error(error);
+};
